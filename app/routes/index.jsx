47c8f8b1d666cd7e2d21a53c1124e568b59af8c3
Moinul Moin <moinulmoin101@gmail.com>
2022-06-03 23:23:29 +0600

    intial commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from '@remix-run/react';
+
+export default function Index() {
+	return (
+		<div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+			<h1>Hello world from Moinul Moin</h1>
+			<div className='mx-auto mt-16 max-w-7xl text-center'>
+				<Link to='/posts' className='text-xl text-blue-600 underline'>
+					Blog Posts
+				</Link>
+			</div>
+		</div>
+	);
+}
